89d7c4ce0a8d5b87f5dc157af3d67d7c72859a60
Tayfur PARMAK <tayfur-parmak-1907@hotmail.com>
2023-06-07 10:08:27 +0300

    newblog

---
+++ kullanici.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 07 Haz 2023, 09:02:16
+-- Sunucu sürümü: 10.4.28-MariaDB
+-- PHP Sürümü: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `blogdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kullanici`
+--
+
+CREATE TABLE `kullanici` (
+  `id` int(11) NOT NULL,
+  `ad` varchar(50) NOT NULL,
+  `soyad` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `kullanici`
+--
+
+INSERT INTO `kullanici` (`id`, `ad`, `soyad`) VALUES
+(1, 'raafs', 'asf'),
+(2, 'asda', 'asda'),
+(3, 'asda', 'asda'),
+(4, 'asda', 'asda'),
+(5, 'asda', 'asda'),
+(6, 'asda', 'asda');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `kullanici`
+--
+ALTER TABLE `kullanici`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kullanici`
+--
+ALTER TABLE `kullanici`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
